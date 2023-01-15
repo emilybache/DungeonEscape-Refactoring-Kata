0929022fe45abff6234de6e749b96f3c3b51fc81
Emily Bache <emily@bacheconsulting.com>
2023-01-15 09:09:52 +0000

    initial import

---
+++ C/main.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void cell_choices();
+void right_corridor();
+
+void play_again() {
+    printf("Play again? y/n\n");
+    int command;
+    while ((command = getchar()) != EOF) {
+        switch (command) {
+            case 10:
+                break;
+            case 'y':
+                printf("play again\n");
+                cell_choices();
+                break;
+            case 'n':
+                printf("quit\n");
+                printf("Thankyou for playing");
+                exit(0);
+                break;
+
+            case 'q':
+                printf("Quit\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                       command, command, isgraph(command) ? command : '.');
+                break;
+        }
+    }
+
+}
+
+void hide() {
+    char* scenario = "You are hiding in the guardroom. Slowly the screams get quieter. However you realize the room was not empty after all. A guard appears and captures you. Soon you find yourself back in your cell, feeling miserable that your escape attempt failed.\n";
+    printf("%s", scenario);
+    play_again();
+}
+
+void guardroom() {
+    char* scenario = "You are standing in an empty guard room that looks to have been hastily abandoned. There is no-one here but the screams grow louder. Suddenly the light from the torch outside goes out.\n";
+    char* choices = "\n\t go back (o)ut of the guardroom\n\t (h)ide in the guardroom\n\n";
+    printf("%s. Would you like to:%s", scenario, choices);
+    int command;
+    while ((command = getchar()) != EOF) {
+        switch (command) {
+            case 10:
+                break;
+            case 'o':
+                printf("out of the guardroom\n");
+                right_corridor();
+                break;
+            case 'h':
+                printf("hide\n");
+                hide();
+                break;
+
+            case 'q':
+                printf("Quit\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                       command, command, isgraph(command) ? command : '.');
+                break;
+        }
+    }
+}
+
+void upstairs() {
+    printf("The stairs lead to the dungeon exit. Your friend Freja the Magnificent Warrior runs towards you and embraces you. 'I am here to rescue you!' she says.\nCongratulations! You have won!!\n\n");
+    play_again();
+}
+
+void left_corridor() {
+    char* scenario = "You are standing at the bottom of some stairs in the dungeon. There is a torch burning on your left and a closed door on your right. You hear screams behind you.";
+    char* choices = "\n\t go (u)p the stairs\n\t go (b)ack\n\t go (t)hrough the door\n\n";
+    printf("%s. Would you like to:%s", scenario, choices);
+    int command;
+    while ((command = getchar()) != EOF) {
+        switch (command) {
+            case 10:
+                break;
+            case 'u':
+                printf("up the stairs\n");
+                upstairs();
+                break;
+            case 'b':
+                printf("go back\n");
+                right_corridor();
+                break;
+            case 't':
+                printf("go through the door\n");
+                guardroom();
+                break;
+
+            case 'q':
+                printf("Quit\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                       command, command, isgraph(command) ? command : '.');
+                break;
+        }
+    }
+
+}
+
+
+void right_corridor() {
+
+    char* scenario = "You are standing in a corridor in the dungeon. It is very dark. A man runs towards you screaming, carrying a big sword";
+    char* choices = "\n\t (r)un the other way\n\t (f)ight him\n\n";
+    printf("%s. Would you like to:%s", scenario, choices);
+    int command;
+    while ((command = getchar()) != EOF) {
+        switch (command) {
+            case 10:
+                break;
+            case 'r':
+                printf("run\n");
+                left_corridor();
+                break;
+            case 'f':
+                printf("fight\n");
+                printf("Unfortunately you lose the fight and fall to the floor dead.\n");
+                play_again();
+                break;
+
+            case 'q':
+                printf("Quit\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                       command, command, isgraph(command) ? command : '.');
+                break;
+        }
+    }
+}
+
+void door_choices() {
+    char* choices = "You are standing in a corridor in the dungeon. To the left you can see a torch burning and some steps. To the right it is dark and you hear screams. Would you like to \n\t go (l)eft\n\t go (r)ight\n\n";
+    printf("%s", choices);
+    int command;
+
+    while ((command = getchar()) != EOF) {
+        switch (command) {
+            case 10:
+                break;
+            case 'l':
+                printf("go left\n");
+                left_corridor();
+                break;
+            case 'r':
+                printf("go right\n");
+                right_corridor();
+                break;
+
+            case 'q':
+                printf("Quit\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                       command, command, isgraph(command) ? command : '.');
+                break;
+        }
+    }
+
+}
+
+void cell_choices() {
+    char* choices = "You are standing in a cell in the dungeon. The recent earthquake has broken the door. Would you like to \n\t (s)tay in the cell\n\t go through the (d)oor\n\n";
+    printf("%s", choices);
+    int command;
+
+    while ((command = getchar()) != EOF) {
+        switch (command) {
+            case 10:
+                break;
+            case 's':
+                printf("stay in the cell\n");
+                cell_choices();
+                break;
+            case 'd':
+                printf("go through the door\n");
+                door_choices();
+                break;
+
+            case 'q':
+                printf("Quit\n");
+                exit(0);
+                break;
+
+            default:
+                printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                       command, command, isgraph(command) ? command : '.');
+                break;
+        }
+    }
+
+}
+
+int main() {
+    printf("Press q to quit at any time.\n\n");
+
+    cell_choices();
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Dungeon Escape Test Design Kata
+================================
+
+How can you design tests for a command-line program that needs user input? Try out your testing skills on this Dungeon Escape adventure game.
+
