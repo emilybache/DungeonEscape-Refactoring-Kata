c1467e3c7ec42daab4ca8eb70440aaca5627987c
Emily Bache <emily@bacheconsulting.com>
2023-01-16 14:25:30 +0000

    hiding and searching

--- C/main.c
+++ C/main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/stat.h>   // stat
+#include <stdbool.h>    // bool type
+#include <unistd.h>
 
 void cell_choices();
 void right_corridor();
+void guardroom();
+
+bool file_exists(char *filename) {
+    struct stat buffer;
+    return (stat(filename, &buffer) == 0);
+}
+
+void write_location(char *location) {
+    FILE *fp = fopen("location.txt", "a");
+    fprintf(fp, "%s\n", location);
+    fclose(fp);
+}
+
+void delete_location() {
+    remove ("location.txt");
+}
 
 void play_again() {
     printf("Play again? y/n\n");
@@ -36,15 +55,81 @@ void play_again() {
 
 }
 
-void hide() {
-    char* scenario = "You are hiding in the guardroom. Slowly the screams get quieter. However you realize the room was not empty after all. A guard appears and captures you. Soon you find yourself back in your cell, feeling miserable that your escape attempt failed.\n";
-    printf("%s", scenario);
-    play_again();
+void search() {
+    char *scenario = "You are searching the guardroom.";
+    printf("%s\n", scenario);
+    if (file_exists("location.txt")) {
+        scenario = "You have found the escaped prisonor! Take them back to their cell. Contragulations, you have won!";
+        printf("%s\n", scenario);
+        delete_location();
+        play_again();
+    } else {
+        scenario = "There is no-one here";
+        printf("%s\n", scenario);
+        guardroom();
+    }
 }
 
+void hiding_loop() {
+    int counter = 0;
+    char *scenario;
+    while (file_exists("location.txt") && counter < 5) {
+        scenario = "The screams are quieter.";
+        printf("%s\n", scenario);
+        sleep(5);
+        counter++;
+    }
+}
+
+void hide() {
+
+    char *scenario = "You are hiding in the guardroom. Press h to stop hiding.";
+    printf("%s\n", scenario);
+    write_location("guardroom");
+    int command;
+    hiding_loop();
+    if (file_exists("location.txt")) {
+        scenario = "You are hiding in the guardroom.";
+        char *choices = "\n\t go back (o)ut of the guardroom\n\t Continue (h)iding\n\n";
+        printf("%s. Would you like to:%s", scenario, choices);
+        while ((command = getchar()) != EOF) {
+            switch (command) {
+                case 10:
+                    break;
+                case 'o':
+                    printf("out of the guardroom\n");
+                    delete_location();
+                    right_corridor();
+                    break;
+                case 'h':
+                    printf("hide\n");
+                    hide();
+                    break;
+
+                case 'q':
+                    printf("Quit\n");
+                    exit(0);
+                    break;
+
+                default:
+                    printf("Unexpected input %d (0x%.2X) ('%c')\n",
+                           command, command, isgraph(command) ? command : '.');
+                    break;
+            }
+        }
+    } else {
+        scenario = "You realize the room is not empty any more. A guard appears and captures you. Soon you find yourself back in your cell, feeling miserable that your escape attempt failed.\n";
+        printf("%s", scenario);
+        play_again();
+    }
+
+}
+
+
+
 void guardroom() {
-    char* scenario = "You are standing in an empty guard room that looks to have been hastily abandoned. There is no-one here but the screams grow louder. Suddenly the light from the torch outside goes out.\n";
-    char* choices = "\n\t go back (o)ut of the guardroom\n\t (h)ide in the guardroom\n\n";
+    char *scenario = "You are standing in an empty guard room that looks to have been hastily abandoned. There is no-one here but the screams grow louder. Suddenly the light from the torch outside goes out.\n";
+    char *choices = "\n\t go back (o)ut of the guardroom\n\t (h)ide in the guardroom\n\t (s)earch the guardroom\n\n";
     printf("%s. Would you like to:%s", scenario, choices);
     int command;
     while ((command = getchar()) != EOF) {
@@ -53,14 +138,19 @@ void guardroom() {
                 break;
             case 'o':
                 printf("out of the guardroom\n");
+                delete_location();
                 right_corridor();
                 break;
             case 'h':
                 printf("hide\n");
                 hide();
                 break;
-
+            case 's':
+                printf("search\n");
+                search();
+                break;
             case 'q':
+                delete_location();
                 printf("Quit\n");
                 exit(0);
                 break;
@@ -74,13 +164,13 @@ void guardroom() {
 }
 
 void upstairs() {
-    printf("The stairs lead to the dungeon exit. Your friend Freja the Magnificent Warrior runs towards you and embraces you. 'I am here to rescue you!' she says.\nCongratulations! You have won!!\n\n");
+    printf("The stairs lead to the dungeon exit. Your friend Freija the Magnificent Warrior runs towards you and embraces you. 'I am here to rescue you!' she says.\nCongratulations! You have won!!\n\n");
     play_again();
 }
 
 void left_corridor() {
-    char* scenario = "You are standing at the bottom of some stairs in the dungeon. There is a torch burning on your left and a closed door on your right. You hear screams behind you.";
-    char* choices = "\n\t go (u)p the stairs\n\t go (b)ack\n\t go (t)hrough the door\n\n";
+    char *scenario = "You are standing at the bottom of some stairs in the dungeon. There is a torch burning on your left and a closed door on your right. You hear screams behind you.";
+    char *choices = "\n\t go (u)p the stairs\n\t go (b)ack\n\t go (t)hrough the door\n\n";
     printf("%s. Would you like to:%s", scenario, choices);
     int command;
     while ((command = getchar()) != EOF) {
@@ -117,8 +207,8 @@ void left_corridor() {
 
 void right_corridor() {
 
-    char* scenario = "You are standing in a corridor in the dungeon. It is very dark. A man runs towards you screaming, carrying a big sword";
-    char* choices = "\n\t (r)un the other way\n\t (f)ight him\n\n";
+    char *scenario = "You are standing in a corridor in the dungeon. It is very dark. A man runs towards you screaming, carrying a big sword";
+    char *choices = "\n\t (r)un the other way\n\t (f)ight him\n\n";
     printf("%s. Would you like to:%s", scenario, choices);
     int command;
     while ((command = getchar()) != EOF) {
@@ -149,7 +239,7 @@ void right_corridor() {
 }
 
 void door_choices() {
-    char* choices = "You are standing in a corridor in the dungeon. To the left you can see a torch burning and some steps. To the right it is dark and you hear screams. Would you like to \n\t go (l)eft\n\t go (r)ight\n\n";
+    char *choices = "You are standing in a corridor in the dungeon. To the left you can see a torch burning and some steps. To the right it is dark and you hear screams. Would you like to \n\t go (l)eft\n\t go (r)ight\n\n";
     printf("%s", choices);
     int command;
 
@@ -181,7 +271,7 @@ void door_choices() {
 }
 
 void cell_choices() {
-    char* choices = "You are standing in a cell in the dungeon. The recent earthquake has broken the door. Would you like to \n\t (s)tay in the cell\n\t go through the (d)oor\n\n";
+    char *choices = "You are standing in a cell in the dungeon. The recent earthquake has broken the door. Would you like to \n\t (s)tay in the cell\n\t go through the (d)oor\n\n";
     printf("%s", choices);
     int command;
 
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 Dungeon Escape Refactoring Kata
 ================================
 
-Try your luck with this Dungeon Escape game.
+Try your luck with this Dungeon Escape game. There are two ways to win. Either find your friend Freija who rescues you. Alternatively you can find the other escaped prisonor. Note you will need to run two instances of the game for this second scenario, so you have two escaped prisoners at once.
 
-The design of this code involves a little more duplication than we're comfortable with. Before you refactor it you will need tests.
-How can you design tests for a command-line program that needs user input? 
+The design of this code involves a little more duplication than we're comfortable with. Before you refactor it you will need tests. How can you design tests for a command-line program that needs user input? 
 
